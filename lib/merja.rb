cbd107bc824cd3fe329252656d449871273fd8c2
lni_T <developer.lni@gmail.com>
2014-09-28 03:57:52 +0900

    public配下のPathnameを返す基本機能を実装

--- lib/merja.rb
+++ lib/merja.rb
@@ -1,2 +1,30 @@
 module Merja
+  class ForbiddenError < StandardError ; end
+  class NotFoundError < StandardError ; end
+
+  class << self
+    def survey(path)
+      target = target_pathname(path)
+      target = sanitize!(target)
+
+      raise NotFoundError unless target.exist?
+
+      target
+    end
+
+    def target_pathname(path)
+      Pathname.new(accessible_dir + path)
+    end
+
+    def sanitize!(pathname)
+      cleanpath = pathname.cleanpath
+      raise ForbiddenError if cleanpath.to_s !~ %r(^#{accessible_dir.cleanpath.to_s})
+      cleanpath
+    end
+
+    # TODO: configurable
+    def accessible_dir
+      Rails.root + "public/"
+    end
+  end
 end

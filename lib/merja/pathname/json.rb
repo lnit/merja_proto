52acd61e72fc9eff6904ff28a5c864087a7eff25
lni_T <developer.lni@gmail.com>
2014-10-26 03:13:04 +0900

    JSONファイルをhash化して返すメソッドを実装(#2)

--- lib/merja/pathname/json.rb
+++ lib/merja/pathname/json.rb
@@ -1,7 +1,12 @@
+require "json"
+
 module Merja::Pathname
+  # JSONファイルを読み込んで返すだけ
   class Json < Pathname
     def to_hash
-      {}
+      File.open(self.to_s) do |io|
+        JSON.load(io)
+      end
     end
   end
 end
